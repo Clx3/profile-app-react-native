25be225a9d60f9a49924d5298d1547534e6603ee
Teemu <teemu.salminen96@gmail.com>
2019-12-05 19:53:40 +0200

    Add ProfileScreen and LoginScreen

---
+++ ProfileApp/screens/ProfileScreen.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default ProfileScreen = (props) => {
+  return(
+    <View>
+      <Text>
+        Hello PROFILE
+      </Text>
+    </View>
+  );
+}
