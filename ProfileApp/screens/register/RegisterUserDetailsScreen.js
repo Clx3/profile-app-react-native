8651232b9c8cfaa618eca58debfd573bed6a0972
Teemu <teemu.salminen96@gmail.com>
2019-12-20 17:33:22 +0200

    Some styling and basic input to UserDetails

--- ProfileApp/screens/register/RegisterUserDetailsScreen.js
+++ ProfileApp/screens/register/RegisterUserDetailsScreen.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Input } from 'react-native-elements';
 
 export default function RegisterUserDetailsScreen(props) {
   return (
-    <View>
-
+    <View style={styles.container}>
+      <Input
+        label='Description'
+        multiline
+        lines={5}
+        labelStyle={styles.labelStyle}
+        inputStyle={styles.inputStyle}
+        placeholder='Please give a little description about yourself...' />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#303030',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  labelStyle: {
+    color: 'white',
+    marginLeft: 5
+  },
+  inputStyle: {
+    color: 'white'
+  }
+});
