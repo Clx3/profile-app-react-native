5b730b1a09e3724badc373eef84cf67036b24b06
Teemu <teemu.salminen96@gmail.com>
2020-01-07 19:51:08 +0200

    Friend search and friend adding implemented

--- ProfileApp/screens/app/friends/FriendsScreen.js
+++ ProfileApp/screens/app/friends/FriendsScreen.js
@@ -1,24 +1,61 @@
 import React, { Component, useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Text, Input } from 'react-native-elements';
+import { View, StyleSheet, FlatList } from 'react-native';
+import { Text, Input, ListItem } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import CustomInput from '../../../components/CustomInput';
-import { getAllProfilesByUsernameSearch } from '../../../api/Api';
+import { getAllProfilesByUsernameSearch, createFriend } from '../../../api/Api';
 
 export default function FriendsScreen(props) {
   const [newFriendText, setNewFriendText] = useState('');
+  const [listData, setListData] = useState([]); // Results of the friend search
+
+  /**
+   * Callback for FriendSearch component for when the response
+   * is received from the API.
+   * 
+   * @param {*} data 
+   */
+  function afterFriendSearchResponse(data) {
+    setListData(data);
+  }
+
+  async function onAddFriendBtnPressAsync(profile) {
+    try {
+      const response = await createFriend(profile.id);
+
+      console.log(response.data);
+    } catch(error) {
+      // TODO: handle error 409 conflict (Already a friend)
+      console.log(error);
+    }
+  }
 
   return (
     <View style={styles.container}>
       <FriendSearch 
         value={newFriendText}
         onChangeText={(value) => setNewFriendText(value)}
+        afterResponse={(data) => afterFriendSearchResponse(data)}
       />
-      <Text style={styles.header}>Your friends</Text>
+      <Text style={styles.header}>Results</Text>
+      <FlatList 
+        data={listData}
+        keyExtractor={item => `${item.id}`}
+        renderItem={({item}) => 
+          <FriendItem 
+            profile={item}
+            onAddFriendBtnPress={async(profile) => await onAddFriendBtnPressAsync(profile)} />
+        }
+      />
     </View>
   );
 }
 
+/**
+ * Renders the friend search input.
+ * 
+ * @param {*} props 
+ */
 function FriendSearch(props) {
 
   let usernameTimeoutId = null;
@@ -29,7 +66,7 @@ function FriendSearch(props) {
     try {
       const response = await getAllProfilesByUsernameSearch(value);
 
-      console.log(response.data)
+      props.afterResponse(response.data);
     } catch(error) {
       console.log(error)
     }
@@ -45,12 +82,31 @@ function FriendSearch(props) {
   );
 }
 
+/**
+ * Renders an item to the friends search result list.
+ * Basically a list item.
+ * 
+ * @param {*} props 
+ */
+function FriendItem(props) {
+  const { profile, onAddFriendBtnPress } = props;
+
+  return (
+    <ListItem
+      title={profile.username}
+      leftIcon={<Icon name={'address-card'} size={27} color='#00F3B2' />}
+      rightIcon={<Icon name='plus-circle' size={27} color='#00F3B2' onPress={() => onAddFriendBtnPress(profile)} />}
+      subtitle={'hehe'}
+      bottomDivider />
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#061620',
-    alignItems: 'center'
+    //alignItems: 'center'
   },
   header: {
     alignSelf: 'flex-start',
