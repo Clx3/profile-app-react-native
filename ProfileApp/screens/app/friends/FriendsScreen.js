e35417fe18afcd9d614e2ce008ec972f43d7f87d
Teemu <teemu.salminen96@gmail.com>
2020-01-10 02:35:57 +0200

    Added refresh functionality to FriendsScreen

--- ProfileApp/screens/app/friends/FriendsScreen.js
+++ ProfileApp/screens/app/friends/FriendsScreen.js
@@ -1,9 +1,10 @@
 import React, { Component, useState, useEffect } from 'react';
-import { View, StyleSheet, FlatList, Modal, Text} from 'react-native';
+import { View, StyleSheet, FlatList, Modal, Text, RefreshControl} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { getFriends, deleteFriend } from '../../../api/Api';
 import ProfileListItem from '../../../components/ProfileListItem';
 import MessageModal from '../../../components/MessageModal';
+import IconText from '../../../components/IconText';
 
 /**
  * This screen component renders a list of users friends.
@@ -14,6 +15,7 @@ import MessageModal from '../../../components/MessageModal';
  */
 export default function FriendsScreen(props) {
   const [listData, setListData] = useState([]); // Contains list of users friends
+  const [loading, setLoading] = useState(false);
   const [modalVisible, setModalVisible] = useState(false);
   const [modalProfile, setModalProfile] = useState(null);
 
@@ -27,6 +29,10 @@ export default function FriendsScreen(props) {
     }
   }, [modalProfile]);
 
+  const onListRefresh = React.useCallback(() => {
+    setLoading(true);
+    updateListData();
+  }, [loading]);
 
   /**
    * Fetches users friends from the API and sets the
@@ -39,6 +45,8 @@ export default function FriendsScreen(props) {
       setListData(response.data);
     } catch(response) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -53,19 +61,22 @@ export default function FriendsScreen(props) {
 
   return (
     <View style={styles.container}>
-        <FlatList 
-          data={listData}
-          keyExtractor={item => `${item.id}`}
-          renderItem={({item}) => 
-            <ProfileListItem 
-              profile={item}
-              RightIcon={
-                <RightIcons 
-                  profile={item}
-                  onMsgButtonPress={() => onMsgButtonPress(item)}
-                  friendRemoveCallBack={async() => await updateListData()}/>
-              }/>
-          }/>
+      <FlatList 
+        contentContainerStyle={{flex: 1}}
+        data={listData}
+        keyExtractor={item => `${item.id}`}
+        renderItem={({item}) => 
+          <ProfileListItem 
+            profile={item}
+            RightIcon={
+              <RightIcons 
+                profile={item}
+                onMsgButtonPress={() => onMsgButtonPress(item)}
+                friendRemoveCallBack={async() => await updateListData()}/>
+            }/>}
+        refreshControl={<RefreshControl refreshing={loading} onRefresh={onListRefresh} />}
+        ListEmptyComponent={<EmptyList />}
+      />
       <MessageModal 
         visible={modalVisible}
         profile={modalProfile}
@@ -107,12 +118,29 @@ function RightIcons(props) {
   );
 }
 
-
+/**
+ * This will be rendered if the Friend list data
+ * is empty.
+ * 
+ * @param {*} props 
+ */
+function EmptyList(props) {
+  return (
+    <View style={styles.emptyListContainer}>
+      <IconText name='frown-o' size={130} text="You don't have any friends. Head to Add friends section." />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#061620',
+  },
+  emptyListContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   header: {
     alignSelf: 'flex-start',
